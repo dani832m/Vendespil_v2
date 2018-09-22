328d11a1450245bcdf78f7af40596d9d4f3b57ab
Daniel <31951102+dani832m@users.noreply.github.com>
2018-09-22 10:25:31 +0200

    Opdateret med pairsFound og timer

--- memoryscript.js
+++ memoryscript.js
@@ -81,7 +81,6 @@ var resetGuesses = function resetGuesses() {
 };
 
 grid.addEventListener('click', function (event) {
-
   var clicked = event.target;
 
   if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
@@ -109,4 +108,33 @@ grid.addEventListener('click', function (event) {
     }
     previousTarget = clicked;
   }
+
+  document.getElementById('pairsFound').innerHTML = pairsFound;
+
 });
+
+//Alt herunder styrer timeren
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+          alert("Tiden er gået! Du fandt " + pairsFound + " par.");
+          location.reload();
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var oneMinute = 60 * 1,
+      display = document.querySelector('#time');
+  startTimer(oneMinute, display);
+};
